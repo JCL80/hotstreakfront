757fc18236fe4d72c23adbfdd5a97cda33ee0ff9
Jorge <jcambrac@gmail.com>
2025-03-20 18:40:48 -0300

    small change

--- src/app/api/nba/teams/all/route.ts
+++ src/app/api/nba/teams/all/route.ts
@@ -12,7 +12,7 @@ const api = new BalldontlieAPI({ apiKey: process.env.BALL_DONT_LIE_API_KEY });
 export async function GET() {
   try {
     const teams = await api.nba.getTeams();
-    console.log("server request teams" , teams)
+    console.log("server request teams **" , teams)
     return NextResponse.json(teams.data);
   } catch (error) {
     console.error("Error fetching NBA teams", error);
